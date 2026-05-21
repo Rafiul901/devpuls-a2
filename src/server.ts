eb9f63f3342fa2b6b14b4c41631f38e562aafc18
Rafiul901 <rafiuldipu2480@gmail.com>
2026-05-21 14:26:37 +0600

    build the server

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import config from "./config";
+import { pool } from "./db";
+
+
+const startServer = async () => {
+  try {
+    await pool.query("SELECT NOW()");
+
+    console.log("Database connected successfully");
+
+    app.listen(config.port, () => {
+      console.log(`Server running on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
