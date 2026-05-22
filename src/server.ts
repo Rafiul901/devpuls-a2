c61d4d73ed2bdeed8a9b01131be96dabc70e69e3
Rafiul901 <rafiuldipu2480@gmail.com>
2026-05-22 15:46:38 +0600

    database created

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import app from "./app";
 import config from "./config";
 import { pool } from "./db";
+import { dataDB } from "./db/dataDB";
 
 
 const startServer = async () => {
   try {
     await pool.query("SELECT NOW()");
-
     console.log("Database connected successfully");
+
+    await dataDB();
 
     app.listen(config.port, () => {
       console.log(`Server running on port ${config.port}`);
